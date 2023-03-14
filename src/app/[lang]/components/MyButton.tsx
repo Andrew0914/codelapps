9a330052912d5974e3d8007cb736a2cef3a52365
AndrewAlanGM <andrewalangm@gmail.com>
2023-03-13 23:14:04 -0600

    Task/move experimental next app (#2)
    * Migrate: i18n, mdx and projecto new beta app direcotry
    
    * 🔧 Fix: theme changing
    
    * 🧪 Add sample test

---
+++ src/app/[lang]/components/MyButton.tsx
@@ -0,0 +1,5 @@
+export default function MyButton() {
+  return (
+    <button className="bg--primary p--1 round--big"> Simple button </button>
+  );
+}
---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,19 @@
+import "@/styles/globals.scss";
+import { getDictionary } from "../../../get-dictionary";
+import { Locale } from "../../../i18n-config";
+import HelloWorld from "./hello.mdx";
+
+export default async function IndexPage({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
+  return (
+    <div>
+      <h1 className="header--0">Current locale: {lang}</h1>
+      <p>This text is rendered on the server:{dictionary.hello}</p>
+      <HelloWorld />
+    </div>
+  );
+}
