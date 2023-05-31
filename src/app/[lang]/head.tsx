bbb5354c4c7696101330cecf9d75a17570ff41e5
AndrewG <andrewalangm@gmail.com>
2023-05-30 23:35:42 -0500

    Layout home page elements

--- src/app/[lang]/head.tsx
+++ src/app/[lang]/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>🍕 Codelapps by @Andrew_GMx</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
         content="This is my personal site to share my experieces as a Developer and a geek in neneral: videogames, code, livestyle"
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,39 @@
 import "@/styles/_globals.scss";
 import { getDictionary } from "../../../get-dictionary";
 import { Locale } from "../../../i18n-config";
+import SocialNetworks from "../components/SocialNetworks";
+import TagsBox from "../components/ui/TagsBox";
+import PostItem, { PostItemProps } from "../components/ui/PostItem";
+import PostsList from "../components/ui/PostsList";
 
+const posts: PostItemProps[] = [
+  {
+    date: "05 May 2023",
+    title: "Pruebas Unitarias",
+    excerpt: "Test de pruebas unitarias",
+    author: "Andrew GM",
+    avatar: "",
+    thumbnail: "",
+  },
+  {
+    date: "05 May 2023",
+    title: "Pruebas Unitarias",
+    excerpt: "Test de pruebas unitarias",
+    author: "Andrew GM",
+    avatar: "",
+    thumbnail: "",
+  },
+  {
+    date: "05 May 2023",
+    title: "Pruebas Unitarias",
+    excerpt: "Test de pruebas unitarias",
+    author: "Andrew GM",
+    avatar: "",
+    thumbnail: "",
+  },
+];
+
+// TODO: get fixed texts from dictionary en - es
 export default async function IndexPage({
   params: { lang },
 }: {
@@ -9,9 +41,21 @@ export default async function IndexPage({
 }) {
   const dictionary = await getDictionary(lang);
   return (
-    <div>
-      <h1 className="header--0">Current locale: {lang}</h1>
-      <p>This text is rendered on the server: {dictionary.hello}</p>
+    <div className="flex--center-column">
+      <section className="w--full bg--lead">
+        <div className="content flex--center-column pt--5 pb--3">
+          <h1 className="header--0 text--center">Hola, soy Andrew GM 👋</h1>
+          <p className="text--content text--md container--xs text--center mt--4">
+            Front-end, Back-end, apps móviles, juegos y más. ¡Bienvenido a mi
+            rincón digital donde la creatividad y la tecnología se fusionan!
+          </p>
+          <SocialNetworks mode="short" className="mt--4" />
+        </div>
+      </section>
+
+      <section className="content py--4">
+        <PostsList posts={posts} />
+      </section>
     </div>
   );
 }
