6ff901cfe45c30628357a1092cc9a940752e2af1
AndrewG <andrewalangm@gmail.com>
2023-06-01 19:18:48 -0500

    📜 Create stories for context posts components

---
+++ src/app/components/PostsViewSelector/PostsViewSelector.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import PostsViewSelector, {
+  PostsViewSelectorProps,
+} from "@/app/components/PostsViewSelector";
+
+export default {
+  title: "Components/PostsViewSelector",
+  component: PostsViewSelector,
+} as ComponentMeta<typeof PostsViewSelector>;
+
+export const Default: ComponentStory<typeof PostsViewSelector> = (
+  args: PostsViewSelectorProps
+) => <PostsViewSelector {...args} />;
+
+Default.args = {};
